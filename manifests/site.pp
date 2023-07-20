ceac375bd00a7b993bc581bc3183e64c35ebf36d
SmirnovAlexey <alexey.work@tut.by>
2023-07-20 21:28:21 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -15,3 +15,21 @@ node 'slave1.puppet' {
         enable => true,
   }
 }
+
+node 'slave2.puppet' {
+  package {
+    'httpd':
+        name => httpd,
+        ensure => installed,
+  }
+-> file { 'index.html':
+          ensure => file,
+          path   => "/var/www/html/index.html",
+          source => "/vagrant/slave/index.html"
+ }
+-> service {
+    'httpd':
+        ensure => true,
+        enable => true,
+  }
+}
